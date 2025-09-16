3372bcb54984f29dc04cd0e69effc56909fcb1fc
thebloxer231 <jicekonemo@gmail.com>
2025-09-16 09:55:43 -0600

    Update script.js    or dedate? Removed a few lines

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 What? No, this isn't actually a script file.
-For your inconvienence, all of the code has been neatly* bundled in the nice little index.html over there.
+For your inconvienence, all of the code has been neatly bundled in the nice little index.html over there.
 No, not here in "script.js"...
 It's over in "index.html".
-
-BTW BloxBox 0.5 or something will probably be a formatting update for index.html, so it becomes WAYYY more easy to use.
-
-
-  
-*lies
